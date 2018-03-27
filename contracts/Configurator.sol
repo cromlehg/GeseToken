cdc1ee8c78c08f867954208495bba59a08e79b20
natyur <natalia.yurkovets@gmail.com>
2018-03-27 15:45:27 +0300

    chore(Configurator): update Configurator

--- contracts/Configurator.sol
+++ contracts/Configurator.sol
@@ -24,33 +24,37 @@ contract Configurator is Ownable {
     preITO.setPeriod(30);
     preITO.setPrice(786700);
     preITO.setMinInvestedLimit(100000000000000000);
-    preITO.setToken(token);
     preITO.setHardcap(3818000000000000000000);
     preITO.setSoftcap(3640000000000000000000);
+    preITO.setReferalsMinInvestLimit(100000000000000000);
+    preITO.setRefererPercent(5);
+    preITO.setToken(token);
 
     token.setSaleAgent(preITO);
 
     ito = new ITO();
 
+    ito.setWallet(0x98882D176234AEb736bbBDB173a8D24794A3b085);
+    ito.setStart(1525910400);
     ito.addMilestone(5, 33);
     ito.addMilestone(5, 18);
     ito.addMilestone(5, 11);
     ito.addMilestone(5, 5);
     ito.addMilestone(5, 0);
-    ito.setMinInvestedLimit(100000000000000000);
-    ito.setToken(token);
     ito.setPrice(550000);
-    ito.setWallet(0x98882D176234AEb736bbBDB173a8D24794A3b085);
+    ito.setMinInvestedLimit(100000000000000000);
+    ito.setHardcap(49090000000000000000000);
     ito.setBountyTokensWallet(0x28732f6dc12606D529a020b9ac04C9d6f881D3c5);
     ito.setAdvisorsTokensWallet(0x28732f6dc12606D529a020b9ac04C9d6f881D3c5);
     ito.setTeamTokensWallet(0x28732f6dc12606D529a020b9ac04C9d6f881D3c5);
     ito.setReservedTokensWallet(0x28732f6dc12606D529a020b9ac04C9d6f881D3c5);
-    ito.setStart(1525910400);
-    ito.setHardcap(49090000000000000000000);
     ito.setBountyTokensPercent(5);
     ito.setAdvisorsTokensPercent(10);
     ito.setTeamTokensPercent(10);
     ito.setReservedTokensPercent(10);
+    ito.setReferalsMinInvestLimit(100000000000000000);
+    ito.setRefererPercent(5);
+    ito.setToken(token);
 
     preITO.setNextSaleAgent(ito);
 
